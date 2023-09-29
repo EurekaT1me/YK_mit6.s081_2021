f24e64679e8dc7dc09746f96700728a4021b0629
Yikai_Zhang <646190204@qq.com>
2023-09-29 13:32:51 +0800

    start lab2: syscall

--- user/ls.c
+++ user/ls.c
@@ -25,51 +25,40 @@ fmtname(char *path)
 void
 ls(char *path)
 {
-    // path: 需要查看的目录路径
   char buf[512], *p;
   int fd;
-  struct dirent de; // 目录文件结构体
-  struct stat st; // 普通文件结构体
+  struct dirent de;
+  struct stat st;
 
-  // 路径不存在
   if((fd = open(path, 0)) < 0){
     fprintf(2, "ls: cannot open %s\n", path);
     return;
   }
 
-  // 获取文件状态失败
   if(fstat(fd, &st) < 0){
     fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
-  // 根据path路径对应的文件类型进行操作
-  // 普通文件: 显示 文件名 - 文件类型编号 - 文件inode编号 - 文件大小
-  // 目录文件:
   switch(st.type){
   case T_FILE:
     printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
     break;
 
   case T_DIR:
-      // 目录路径太长，目录长度+目录下某个子文件最小长度（14）> 512Bytes
     if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-      printf("find: path too long\n");
+      printf("ls: path too long\n");
       break;
     }
-
     strcpy(buf, path);
-    p = buf+strlen(buf); // 定位到path最后一个字符位置
+    p = buf+strlen(buf);
     *p++ = '/';
-    // 读取目录文件下的结构
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
       if(de.inum == 0)
         continue;
-      // memmove，防止memcpy可能的地址覆盖问题
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
-      // 读取文件结构
       if(stat(buf, &st) < 0){
         printf("ls: cannot stat %s\n", buf);
         continue;
